edab9b4a332a1ee0b56f4d129fc293ee83df5269
NurseRo <rvar14@student.monash.edu>
2016-08-05 18:38:02 +1000

    Preloading screen now animated to move itself upon page load.

--- scripts/loading.js
+++
@@ -1,9 +0,0 @@
-function loaded(show_page){
-    var loader;
-    loader = document.getElementById("loading");
-    show = document.getElementById(show_page)
-    // Hide loading layer
-    loader.style.display = "none";    
-    // Show the intended page
-    show.style.display = "block";
-}
